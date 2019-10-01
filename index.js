e2a50bd72114db8c94c156a5dd8297df5658bc2b
noahfranco <noahfra@gmail.com>
2019-10-01 15:46:10 -0400

    finished .post()

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ const server = express();
 
 server.use(express.json())
 
-server.get("/", (req, res) => {
-    res.send("It's working")
+
+server.post("/api/posts", (req, res) => {
+    const post = req.body
+
+   if(!title && !contents) {
+       res.status(400).json({errorMessage: "Please provide title and contents for the post."})   
+   } else {
+       db 
+        .find(post)
+       .then(posted => {
+           res.status(201).json(posted)
+       })
+       .catch(error => {
+           console.log(error)
+           res.status(500).json({error: "There was an error while saving the post to the database"})
+       })
+   }
 })
 
 server.listen(4000, () => {
