d8ec14cc63b3f388f734770af3a035d9c7b5f7c3
noahfranco <noahfra@gmail.com>
2019-10-01 23:56:48 -0400

    .delete() is working

--- dbRouter.js
+++ dbRouter.js
@@ -25,27 +25,6 @@ router.post("/", (req, res) => {
    }
 })
 
-// Having a hard time getting .post() with ID to work
-// .post() with ID
-// router.post("/:id/comments", (req, res) => {
-//     const { id }  = req.params; 
-//     if(!id) {
-//         res.status(404).json({message: "The post with the specified ID does not exist"})
-//     } else  {
-//         res.status(400).json({errorMessage: "Please provide text for the comment"})
-//         db
-//         .insert(id)
-//         .then(created => {
-//             res.status(201).json(created)
-//         })
-        
-//         .catch(error => {
-//             console.log(error)
-//             res.status(500).json({error: "There was an error while saving the comment to the database"})
-//         })
-//     } 
-//     })
-
 
 // .get() 
 router.get("/", (req, res) => {
@@ -79,8 +58,49 @@ router.get("/:id", (req, res) => {
     }
 })
 
-// 
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    if(!id) {
+        res.status(404).json({message: "The post with the specified ID does not exist"})
+    } else {
+        db
+        .remove(id)
+        .then(gone => {
+            res.status(202).json(gone)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({error: "The post could not be removed"})
+        })
+    }
+})
 
+// *************
+// BY COMMENTS
+// *************
+
+ // Having a hard time getting .post() with ID to work
+// .post() with ID
+// router.post("/:id/comments", (req, res) => {
+//     const { id }  = req.params; 
+//     if(!id) {
+//         res.status(404).json({message: "The post with the specified ID does not exist"})
+//     } else  {
+//         res.status(400).json({errorMessage: "Please provide text for the comment"})
+//         db
+//         .insert(id)
+//         .then(created => {
+//             res.status(201).json(created)
+//         })
+        
+//         .catch(error => {
+//             console.log(error)
+//             res.status(500).json({error: "There was an error while saving the comment to the database"})
+//         })
+//     } 
+//     })
+
+// .get() by comments 
 
 
 module.exports = router; 
